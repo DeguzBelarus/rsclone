0e73365966186d37a346e0df0408ee7e390f2200
DeguzBelarus <deguz@mail.ru>
2023-02-02 14:20:07 +0300

    feat: add empty page 404 and update router

--- client/src/router/useRoutes.tsx
+++ client/src/router/useRoutes.tsx
@@ -1,15 +1,16 @@
-import { AuthPage } from 'pages/AuthPage/AuthPage';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { MainPage } from '../pages/MainPage/MainPage';
+import { AuthPage } from 'pages/AuthPage/AuthPage';
+import { Page404 } from 'pages/Page404/Page404';
 
 export const useRoutes = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />}></Route>
-      <Route path="*" element={<MainPage />}></Route>
       <Route path="login" element={<AuthPage />}></Route>
+      <Route path="*" element={<Page404 />}></Route>
     </Routes>
   );
 };
