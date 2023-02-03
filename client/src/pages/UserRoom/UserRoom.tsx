ae05fe615d8f05fcc9f1227c25b16a5b56085f02
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-02-03 01:08:38 +0100

    feat: display user data on userRoom page

--- client/src/pages/UserRoom/UserRoom.tsx
+++ client/src/pages/UserRoom/UserRoom.tsx
@@ -1,7 +1,23 @@
+import { useAppSelector } from 'app/hooks';
+import { getIsAuthorized, getUserEmail, getUserNickname } from 'app/mainSlice';
 import React, { FC } from 'react';
 
 import './UserRoom.scss';
 
 export const UserRoom: FC = (): JSX.Element => {
-  return <div className="user-room-wrapper">UserRoom works!</div>;
+  const isAuthorized = useAppSelector(getIsAuthorized);
+  const userEmail = useAppSelector(getUserEmail);
+  const userNickname = useAppSelector(getUserNickname);
+
+  return (
+    <div className="user-room-wrapper">
+      <div>UserRoom works!</div>
+      {isAuthorized && (
+        <div className="user-data">
+          <div>Email: {userEmail}</div>
+          <div>Nickname: {userNickname}</div>
+        </div>
+      )}
+    </div>
+  );
 };
