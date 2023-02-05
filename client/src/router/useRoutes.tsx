dbf99d7185dc7d0ae84a6e04ea18ecadb0010225
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-02-05 16:43:55 +0100

    refactor: remove all external components from useRoutes

--- client/src/router/useRoutes.tsx
+++ client/src/router/useRoutes.tsx
@@ -9,27 +9,21 @@ import { PostPage } from 'pages/PostPage/PostPage';
 import { PrivateRoutes } from 'components/PrivateRoutes';
 import { PublicRoutes } from 'components/PublicRoutes';
 import { AuthPage } from 'pages/AuthPage/AuthPage';
-import { Alert } from 'components/Alert/Alert';
-import { Header } from 'components/Header/Header';
 
 export const useRoutes = (): JSX.Element => {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/" element={<UserRoom />} />
-          <Route path="settings" element={<UserSettings />} />
-          <Route path="post/:id" element={<PostPage />} />
-        </Route>
+    <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<UserRoom />} />
+        <Route path="settings" element={<UserSettings />} />
+        <Route path="post/:id" element={<PostPage />} />
+      </Route>
 
-        <Route element={<PublicRoutes />}>
-          <Route path="login" element={<AuthPage />} />
-          <Route path="register" element={<RegisterPage />} />
-        </Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-      <Alert />
-    </>
+      <Route element={<PublicRoutes />}>
+        <Route path="login" element={<AuthPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 };
