d030864166c81386e6584160e37903fac5b75a31
DeguzBelarus <deguz@mail.ru>
2023-02-08 20:28:26 +0300

    feat: add processing page in routing

---
+++ client/src/pages/ProcessingPage/ProcessingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ProcessingPage.scss';
+
+export const ProcessingPage = () => {
+  return (
+    <div className="processing-wrapper">
+      <h2 className="processing-heading">Processing...</h2>
+    </div>
+  );
+};
--- client/src/router/useRoutes.tsx
+++ client/src/router/useRoutes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { RegisterPage } from 'pages/RegisterPage/RegisterPage';
+import { ProcessingPage } from 'pages/ProcessingPage/ProcessingPage';
 import { Page404 } from 'pages/Page404/Page404';
 import { UserRoom } from 'pages/UserRoom/UserRoom';
 import { UserSettings } from '../pages/UserSettings/UserSettings';
@@ -9,10 +10,12 @@ import { AuthPage } from 'pages/AuthPage/AuthPage';
 import { Socket } from 'socket.io-client';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { useAppSelector } from 'app/hooks';
-import { getIsAuthorized, getUserId } from 'app/mainSlice';
+import { getIsAuthorized, getUserId, getUserRequestStatus } from 'app/mainSlice';
 
 export const useRoutes = (socket: Socket<DefaultEventsMap, DefaultEventsMap>) => {
   const isAuthorized = useAppSelector(getIsAuthorized);
+  const userRequestStatus = useAppSelector(getUserRequestStatus);
+
   const userId = useAppSelector(getUserId);
 
   return (
@@ -27,14 +30,20 @@ export const useRoutes = (socket: Socket<DefaultEventsMap, DefaultEventsMap>) =>
           <Route path="settings" element={<UserSettings socket={socket} />} />
           <Route path="post/:id" element={<PostPage />} />
           <Route path="post/*" element={<PostPage />} />
-          <Route path="*" element={<Page404 />} />
+          <Route
+            path="*"
+            element={userRequestStatus === 'loading' ? <ProcessingPage /> : <Page404 />}
+          />
         </>
       ) : (
         <>
           <Route path="/" element={<Navigate to={'/login'} />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<AuthPage />} />
-          <Route path="*" element={<Page404 />} />
+          <Route
+            path="*"
+            element={userRequestStatus === 'loading' ? <ProcessingPage /> : <Page404 />}
+          />
         </>
       )}
     </Routes>
