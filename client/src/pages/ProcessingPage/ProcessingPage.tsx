ec6223cc9b9c7bc967b70d8ce55bb15ea0e36f9c
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2023-02-13 03:43:36 +0300

    refactor: upgrade spinner and processing page

--- client/src/pages/ProcessingPage/ProcessingPage.tsx
+++ client/src/pages/ProcessingPage/ProcessingPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Spinner } from '../../components/Spinner/Spinner';
 
 import './ProcessingPage.scss';
 
 export const ProcessingPage = () => {
   return (
     <div className="processing-wrapper">
+      <Spinner />
       <h2 className="processing-heading">Processing...</h2>
     </div>
   );
