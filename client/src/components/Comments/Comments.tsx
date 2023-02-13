52fafe8ccfe78435af0efc1eb5c48db67880f9fd
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-02-13 13:34:15 +0100

    feat: implement comments component

---
+++ client/src/components/Comments/Comments.tsx
@@ -0,0 +1,127 @@
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import React, { useState } from 'react';
+import { createCommentAsync, getCurrentLanguage, getToken, getUserId } from 'app/mainSlice';
+import useLanguage from 'hooks/useLanguage';
+import { lng } from 'hooks/useLanguage/types';
+import styles from './Comments.module.scss';
+import { ICommentModel } from 'types/types';
+import {
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Tooltip,
+} from '@mui/material';
+import { Send as SendIcon, Clear as ClearIcon } from '@mui/icons-material';
+import useValidateInput from 'hooks/useValidateInput';
+import Avatar from 'components/Avatar';
+
+interface CommentsProps {
+  postId?: number;
+  data?: ICommentModel[];
+  onChange?: () => void;
+}
+
+export const Comments = ({ postId, data, onChange }: CommentsProps) => {
+  const dispatch = useAppDispatch();
+  const lang = useAppSelector(getCurrentLanguage);
+  const token = useAppSelector(getToken);
+  const userId = useAppSelector(getUserId);
+  const language = useLanguage();
+
+  const [commentValue, setCommentValue] = useState('');
+  const [commentError, setCommentError] = useState(false);
+  const [touched, setTouched] = useState(false);
+
+  const validateComment = useValidateInput(
+    (value) => value.length < 3,
+    setCommentValue,
+    setCommentError,
+    setTouched
+  );
+
+  const handleAddComment = async () => {
+    const isValid = touched && validateComment(commentValue);
+    // console.log(isValid, token, userId, postId);
+    if (!isValid || !token || !userId || !postId) return;
+
+    const requestData = {
+      lang,
+      token,
+      postId,
+      userId,
+      requestData: {
+        commentText: commentValue,
+      },
+    };
+
+    const result = await dispatch(createCommentAsync(requestData));
+    // console.log(result);
+    if (result.meta.requestStatus === 'fulfilled') {
+      console.log('added');
+      setCommentValue('');
+      setCommentError(false);
+      setTouched(false);
+      if (onChange) onChange();
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>Comments</h2>
+      <div className={styles.add}>
+        <TextField
+          sx={{ width: '100%' }}
+          variant="standard"
+          placeholder="Write your comment here..."
+          value={commentValue}
+          // label={language(lng.postTitle)}
+          // error={commentError}
+          onChange={validateComment}
+          onKeyDownCapture={(event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              handleAddComment();
+            }
+          }}
+          InputProps={{
+            endAdornment: (
+              <>
+                <Tooltip title={language(lng.userAddPost)}>
+                  <span>
+                    <IconButton disabled={commentError} color="inherit" onClick={handleAddComment}>
+                      <SendIcon />
+                    </IconButton>
+                  </span>
+                </Tooltip>
+                {commentValue.length > 0 && (
+                  <Tooltip title={language(lng.userAddPost)}>
+                    <IconButton color="inherit" onClick={handleAddComment}>
+                      <ClearIcon />
+                    </IconButton>
+                  </Tooltip>
+                )}
+              </>
+            ),
+          }}
+        />
+      </div>
+      {data && data?.length > 0 ? (
+        <List className={styles.comments}>
+          {data.map(({ id, commentText }) => (
+            <ListItem key={id}>
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
+              <ListItemText>{commentText}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      ) : (
+        <div>NO Comments added</div>
+      )}
+    </div>
+  );
+};
--- client/src/pages/PostPage/PostPage.tsx
+++ client/src/pages/PostPage/PostPage.tsx
@@ -1,14 +1,15 @@
+import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { getCurrentLanguage, getCurrentPost, getOnePostAsync, setCurrentPost } from 'app/mainSlice';
 import { Post } from 'components/Post/Post';
 import useLanguage from 'hooks/useLanguage';
 import { lng } from 'hooks/useLanguage/types';
 import { Page404 } from 'pages/Page404/Page404';
-import React, { FC, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import styles from './PostPage.module.scss';
+import { Comments } from 'components/Comments/Comments';
 
-export const PostPage: FC = (): JSX.Element => {
+export const PostPage = () => {
   const dispatch = useAppDispatch();
   const post = useAppSelector(getCurrentPost);
   const lang = useAppSelector(getCurrentLanguage);
@@ -24,11 +25,21 @@ export const PostPage: FC = (): JSX.Element => {
     return () => {
       dispatch(setCurrentPost(null));
     };
-  }, [id]);
+  }, [id, lang, dispatch]);
 
   return post ? (
     <div className={styles.wrapper}>
       <Post data={post} single onDelete={() => navigate('/')} />
+      <Comments
+        postId={Number(id)}
+        data={post.comments}
+        onChange={() => {
+          const postId = Number(id);
+          if (!Number.isNaN(postId)) {
+            dispatch(getOnePostAsync({ id: postId, lang }));
+          }
+        }}
+      />
     </div>
   ) : (
     <Page404 message={language(lng.userNotFound).replace('%', id || '')} />
