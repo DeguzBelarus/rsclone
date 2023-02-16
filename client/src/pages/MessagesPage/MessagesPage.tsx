52e86c1c9149e61cbac54c9bb3e0edae2dbe071d
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-02-16 19:00:28 +0100

    feat: implement messages page

--- client/src/pages/MessagesPage/MessagesPage.tsx
+++ client/src/pages/MessagesPage/MessagesPage.tsx
@@ -1,51 +1,48 @@
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import {
+  getChats,
   getCurrentLanguage,
   getDialogs,
   getUserId,
   getUserNickname,
-  setDialogs,
+  setActiveChatId,
+  setChats,
 } from 'app/mainSlice';
 import useLanguage from 'hooks/useLanguage';
 import { lng } from 'hooks/useLanguage/types';
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './MessagesPage.module.scss';
 
 export const MessagesPage = () => {
-  const dispatch = useAppDispatch();
   const language = useLanguage();
+  const dispatch = useAppDispatch();
   const dialogs = useAppSelector(getDialogs);
   const userId = useAppSelector(getUserId);
   const nickname = useAppSelector(getUserNickname);
   const lang = useAppSelector(getCurrentLanguage);
+  const chats = useAppSelector(getChats);
 
-  useEffect(() => {
-    if (!userId || !nickname) return;
-    dispatch(
-      setDialogs([
-        {
-          authorId: userId,
-          authorNickname: nickname,
-          lastMessageAuthorNickname: 'sfsdf',
-          lastMessageDate: String(Date.now()),
-          lastMessageId: 5,
-          lastMessageText: 'Message Text',
-          recipientId: userId,
-          recipientNickname: nickname,
-          unreadMessages: 4,
-        },
-      ])
-    );
-  }, [lang, dispatch, userId, nickname]);
+  const handleStartChat = (partnerId: number, partnerNickname: string) => {
+    const addedChatIndex = chats.findIndex((chat) => partnerId === chat.partnerId);
+    if (addedChatIndex < 0) {
+      dispatch(setChats([...chats, { partnerId, partnerNickname }]));
+    }
+    dispatch(setActiveChatId(partnerId));
+  };
 
   return (
     <div className={styles.wrapper}>
       <h2>{language(lng.messagesHeading)}</h2>
-      <ul>
-        {dialogs.map(({ authorNickname }) => (
-          <li key={authorNickname}>{authorNickname}</li>
+      <List>
+        {dialogs.map(({ recipientId, recipientNickname, unreadMessages }) => (
+          <ListItem key={recipientId} disablePadding>
+            <ListItemButton onClick={() => handleStartChat(recipientId, recipientNickname)}>
+              <ListItemText primary={`${recipientNickname} (${unreadMessages})`} />
+            </ListItemButton>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
