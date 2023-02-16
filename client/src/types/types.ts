995f8d3c3e665c7b1c3f8e7af0e67af18bc45fb8
DeguzBelarus <deguz@mail.ru>
2023-02-16 13:50:07 +0300

    feat: add types for message deleting request/response

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -230,6 +230,19 @@ export interface IUpdatePostResponse {
   statusCode?: number;
 }
 
+export interface IDeleteMessageRequest {
+  lang: CurrentLanguageType;
+  messageId: number;
+  token: string;
+}
+
+export interface IDeleteMessageResponse {
+  message: string;
+  messageOwnerId: number;
+  messageId: number;
+  statusCode?: number;
+}
+
 export interface IGetAllPostsResponse {
   postsData: Array<IPostModel>;
   message: string;
