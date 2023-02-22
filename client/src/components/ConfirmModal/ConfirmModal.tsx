74f93c87d0301b074278bd49276c7aeb2dcb71ca
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-02-22 08:44:07 +0100

    feat: implement add link functionality

--- client/src/components/ConfirmModal/ConfirmModal.tsx
+++ client/src/components/ConfirmModal/ConfirmModal.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useRef, useState } from 'react';
 
-import { Dialog, DialogContent, DialogTitle, DialogActions, Button } from '@mui/material';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  Button,
+  TextField,
+  DialogContentText,
+} from '@mui/material';
 import useLanguage from 'hooks/useLanguage';
 import { lng } from 'hooks/useLanguage/types';
+import useValidateInput from 'hooks/useValidateInput';
 
 export interface ConfirmModalProps {
   open: boolean;
   title?: string;
   closeOnly?: boolean;
-  onSuccess?: () => void;
+  inputLabel?: string;
+  inputInitial?: string;
+  onSuccess?: (value?: string) => void;
   onClose?: () => void;
   children?: React.ReactNode;
 }
@@ -17,35 +28,75 @@ export const ConfirmModal = ({
   open,
   title,
   closeOnly,
+  inputLabel,
+  inputInitial,
   onClose,
   onSuccess,
   children,
 }: ConfirmModalProps) => {
   const langauge = useLanguage();
+  const [inputValue, setInputValue] = useState('');
+  const [inputError, setInputError] = useState(false);
+  const [touched, setTouched] = useState(false);
+  const validateInput = useValidateInput(
+    (value) => value === '',
+    setInputValue,
+    setInputError,
+    setTouched
+  );
+
+  const resetInput = () => {
+    setTouched(false);
+    setInputError(false);
+    setInputValue('');
+  };
 
   const handleClose = () => {
+    resetInput();
     if (onClose) onClose();
   };
 
-  const handleConfirm = () => {
+  const handleConfirm = (event: FormEvent) => {
+    event.preventDefault();
+    if (inputLabel && (!validateInput(inputValue) || !touched)) return;
+    if (onSuccess) onSuccess(inputValue);
     handleClose();
-    if (onSuccess) onSuccess();
   };
+
+  useEffect(() => setInputValue(inputInitial || ''), [inputInitial]);
 
   return (
     <Dialog open={open} disableScrollLock onClose={handleClose}>
       <DialogTitle>{title}</DialogTitle>
-      <DialogContent>{children}</DialogContent>
-      <DialogActions>
-        {closeOnly ? (
-          <Button onClick={handleClose}>{langauge(lng.close)}</Button>
-        ) : (
-          <>
-            <Button onClick={handleClose}>{langauge(lng.cancel)}</Button>
-            <Button onClick={handleConfirm}>{langauge(lng.confirm)}</Button>
-          </>
-        )}
-      </DialogActions>
+      <form onSubmit={handleConfirm}>
+        <DialogContent>
+          <DialogContentText>{children}</DialogContentText>
+          {inputLabel && (
+            <TextField
+              fullWidth
+              autoFocus
+              variant="standard"
+              label={inputLabel}
+              value={inputValue}
+              error={inputError}
+              sx={{ minWidth: 'clamp(180px, 40vw, 600px)' }}
+              onChange={validateInput}
+            />
+          )}
+        </DialogContent>
+        <DialogActions>
+          {closeOnly ? (
+            <Button type="submit" onClick={handleClose}>
+              {langauge(lng.close)}
+            </Button>
+          ) : (
+            <>
+              <Button onClick={handleClose}>{langauge(lng.cancel)}</Button>
+              <Button type="submit">{langauge(lng.confirm)}</Button>
+            </>
+          )}
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
--- client/src/components/RichEditor/RichEditor.tsx
+++ client/src/components/RichEditor/RichEditor.tsx
@@ -10,6 +10,7 @@ import {
   Modifier,
   SelectionState,
   ContentBlock,
+  CompositeDecorator,
 } from 'draft-js';
 import { decompressFromUTF16 } from 'async-lz-string';
 import IconButton from '@mui/material/IconButton';
@@ -23,6 +24,7 @@ import useLanguage from 'hooks/useLanguage';
 import { editorActions } from './RichEditor.consts';
 import styles from './RichEditor.module.scss';
 import './RichEditor.scss';
+import { ConfirmModal } from 'components/ConfirmModal/ConfirmModal';
 
 interface EditorButtonProps {
   style: string;
@@ -31,6 +33,7 @@ interface EditorButtonProps {
   title: string;
   editorState: EditorState;
   toggleStyle: (style: string, block?: boolean) => void;
+  onClick?: () => void;
 }
 
 const EditorButton = ({
@@ -40,33 +43,72 @@ const EditorButton = ({
   title,
   editorState,
   toggleStyle,
+  onClick,
 }: EditorButtonProps) => {
   const [isStyle, setIsStyle] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
+    const selection = editorState.getSelection();
     if (block) {
-      const currentSelection = editorState.getSelection();
-      const currentKey = currentSelection.getStartKey();
+      const currentKey = selection.getStartKey();
       const currentBlock = editorState.getCurrentContent().getBlockForKey(currentKey);
       setIsStyle(currentBlock.getType() === style);
     } else {
       const inlineStyle = editorState.getCurrentInlineStyle();
       setIsStyle(inlineStyle.has(style));
     }
+    setDisabled(style === 'LINK' && selection.isCollapsed());
   }, [editorState, style, block]);
 
   return (
     <Tooltip title={title}>
-      <IconButton
-        size="small"
-        color={isStyle ? 'primary' : 'default'}
-        onClick={() => toggleStyle(style, block)}
-      >
-        {Icon && <Icon />}
-      </IconButton>
+      <span>
+        <IconButton
+          size="small"
+          color={isStyle ? 'primary' : 'default'}
+          disabled={disabled}
+          onClick={() => (onClick ? onClick() : toggleStyle(style, block))}
+        >
+          {Icon && <Icon />}
+        </IconButton>
+      </span>
     </Tooltip>
   );
 };
+
+function findLinkEntities(
+  contentBlock: ContentBlock,
+  callback: (start: number, end: number) => void,
+  contentState: ContentState
+) {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity();
+    return entityKey !== null && contentState.getEntity(entityKey).getType() === 'LINK';
+  }, callback);
+}
+
+interface LinkProps {
+  contentState: ContentState;
+  entityKey: string;
+  children?: React.ReactNode;
+}
+
+const Link = ({ contentState, entityKey, children }: LinkProps) => {
+  const { url } = contentState.getEntity(entityKey).getData();
+  return (
+    <a href={url} className={styles.link}>
+      {children}
+    </a>
+  );
+};
+
+const decorator = new CompositeDecorator([
+  {
+    strategy: findLinkEntities,
+    component: Link,
+  },
+]);
 
 interface RichEditorProps {
   label?: string;
@@ -88,8 +130,12 @@ export const RichEditor = ({
   const { palette } = useTheme();
   const language = useLanguage();
 
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const createEmptyState = () => EditorState.createEmpty(decorator);
+
+  const [editorState, setEditorState] = useState(createEmptyState());
   const [editorFocused, setEditorFocused] = useState(false);
+  const [linkModalOpen, setLinkModalOpen] = useState(false);
+  const [linkAddress, setLinkAddress] = useState('');
   const editorRef: React.LegacyRef<Editor> = useRef(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const isEmpty = editorState.getCurrentContent().hasText();
@@ -122,10 +168,57 @@ export const RichEditor = ({
     }
   };
 
+  const getCurrentLinkAddress = (): string => {
+    const content = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+    const block = content.getBlockForKey(selection.getStartKey());
+    let selectedEntityURL: string | null = null;
+    const entityURLs: Array<string | null> = [];
+    block.findEntityRanges(
+      (character) => {
+        try {
+          const entityKey = character.getEntity();
+          const entity = content.getEntity(entityKey);
+          const entityData = entity.getData();
+          const entityType = entity.getType();
+          if (entityType === 'LINK') {
+            selectedEntityURL = entityData?.url || null;
+            return true;
+          }
+        } catch {
+          return false;
+        }
+        return false;
+      },
+      (start: number, end: number) => {
+        if (selectedEntityURL) entityURLs.push(selectedEntityURL);
+      }
+    );
+    if (entityURLs[0]) return entityURLs[0];
+    const windowSelection = window.getSelection();
+    if (!selection.isCollapsed() && windowSelection) {
+      return windowSelection.toString();
+    }
+    return '';
+  };
+
+  useEffect(() => {
+    setLinkAddress(getCurrentLinkAddress());
+  }, [editorState, getCurrentLinkAddress]);
+
+  const handleLinkClick = () => {
+    const content = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+    // const block = content.getBlockForKey(selection.getAnchorKey());
+    if (!selection.isCollapsed()) setLinkModalOpen(true);
+  };
+
   const handleKeyCommand = (command: string, editorState: EditorState) => {
     const currentStyle = editorActions.find(({ style }) => style === command);
     if (currentStyle) {
-      toggleStyle(command, currentStyle.block);
+      if (currentStyle.style === 'LINK') {
+        handleLinkClick();
+      } else toggleStyle(command, currentStyle.block);
       return 'handled';
     }
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -160,12 +253,28 @@ export const RichEditor = ({
     const anchorKey = selection.getAnchorKey();
     const currentBlockKey = content.getBlockForKey(anchorKey).getKey();
     const anchorOffset = selection.getAnchorOffset();
-    const state = EditorState.createWithContent(Modifier.replaceText(content, selection, emoji));
+    const state = EditorState.createWithContent(
+      Modifier.replaceText(content, selection, emoji),
+      decorator
+    );
     const updatedSelection = SelectionState.createEmpty(currentBlockKey).merge({
       anchorOffset,
       focusOffset: anchorOffset + emoji.length,
     });
     setEditorState(EditorState.forceSelection(state, updatedSelection));
+  };
+
+  const handleAddLink = (value?: string) => {
+    if (!value) return;
+    let url = value;
+    if (!url.startsWith(`http:\/\/`) && !url.startsWith(`https:\/\/`)) {
+      url = `https:\/\/${url}`;
+    }
+    const contentState = editorState.getCurrentContent();
+    const contentStateWithEntity = contentState.createEntity('LINK', 'MUTABLE', { url });
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+    setEditorState(RichUtils.toggleLink(newEditorState, newEditorState.getSelection(), entityKey));
   };
 
   useEffect(() => {
@@ -184,10 +293,10 @@ export const RichEditor = ({
       if (!initialValue) return;
       try {
         const state = JSON.parse(await decompressFromUTF16(initialValue));
-        setEditorState(EditorState.createWithContent(convertFromRaw(state)));
+        setEditorState(EditorState.createWithContent(convertFromRaw(state), decorator));
       } catch (error) {
         const contentState = ContentState.createFromText(initialValue);
-        setEditorState(EditorState.createWithContent(contentState));
+        setEditorState(EditorState.createWithContent(contentState, decorator));
       }
     };
     changeValue();
@@ -206,29 +315,42 @@ export const RichEditor = ({
       )}
     >
       {!readOnly && (
-        <div className={styles.controls}>
-          {editorActions.map(({ style, block, icon, title, shortcut }, index) =>
-            style === 'EMOJI' ? (
-              <EmojiButton key={style} small onEmojiAdded={handleEmojiAdded} />
-            ) : style === 'DIVIDER' ? (
-              <span
-                key={index}
-                className={styles.divider}
-                style={{ backgroundColor: palette.divider }}
-              ></span>
-            ) : (
-              <EditorButton
-                key={style}
-                style={style}
-                block={block}
-                icon={icon}
-                title={`${language(title || lng.formatBold)} (${shortcut})`}
-                editorState={editorState}
-                toggleStyle={toggleStyle}
-              />
-            )
-          )}
-        </div>
+        <>
+          <div className={styles.controls}>
+            {editorActions.map(({ style, block, icon, title, shortcut }, index) =>
+              style === 'EMOJI' ? (
+                <EmojiButton key={style} small onEmojiAdded={handleEmojiAdded} />
+              ) : style === 'DIVIDER' ? (
+                <span
+                  key={index}
+                  className={styles.divider}
+                  style={{ backgroundColor: palette.divider }}
+                ></span>
+              ) : (
+                <EditorButton
+                  key={style}
+                  style={style}
+                  block={block}
+                  icon={icon}
+                  title={`${language(title || lng.formatBold)} (${shortcut})`}
+                  editorState={editorState}
+                  toggleStyle={toggleStyle}
+                  onClick={style === 'LINK' ? handleLinkClick : undefined}
+                />
+              )
+            )}
+          </div>
+          <ConfirmModal
+            open={linkModalOpen}
+            title="Edit link"
+            inputLabel="Link address"
+            inputInitial={linkAddress}
+            onClose={() => setLinkModalOpen(false)}
+            onSuccess={handleAddLink}
+          >
+            Please provide link address:
+          </ConfirmModal>
+        </>
       )}
       <Editor
         ref={editorRef}
