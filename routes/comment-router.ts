5091d4719e4ffe8a798fea5224164f80f447ceeb
DeguzBelarus <deguz@mail.ru>
2023-02-24 16:00:31 +0300

    refactor: refactor code in routers

--- routes/comment-router.ts
+++ routes/comment-router.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 import { commentController } from '../controllers/comment-controller'
 import { roleAndIdAccessMiddleware } from '../middleware/role-id-access';
 
 export const commentRouter = express.Router();
-commentRouter.post("/:postId/:userId/creation", roleAndIdAccessMiddleware, commentController.create);
-commentRouter.delete("/:id/delete", roleAndIdAccessMiddleware, commentController.delete);
-commentRouter.put("/:id/update", roleAndIdAccessMiddleware, commentController.update);
+commentRouter.post('/:postId/:userId/creation', roleAndIdAccessMiddleware, commentController.create);
+commentRouter.delete('/:id/delete', roleAndIdAccessMiddleware, commentController.delete);
+commentRouter.put('/:id/update', roleAndIdAccessMiddleware, commentController.update);
