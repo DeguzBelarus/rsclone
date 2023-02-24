d9e6ce0bee6389f6a3126b4eae9ffe97855bed5c
DeguzBelarus <deguz@mail.ru>
2023-02-24 14:15:54 +0300

    feat: add comments to every post for get all posts request

--- controllers/post-controller.ts
+++ controllers/post-controller.ts
@@ -159,10 +159,10 @@ class PostController {
 
   async getAllPosts(request: Request, response: Response, next: NextFunction) {
     try {
-      if (Post) {
+      if (Post && Comment) {
         const { lang } = request.query;
 
-        const foundPosts = await Post.findAll();
+        const foundPosts = await Post.findAll({ include: [{ model: Comment, as: "comments" }] });
         if (foundPosts) {
           return response.json({
             postsData: foundPosts
@@ -178,6 +178,7 @@ class PostController {
                   ownerNickname: post.dataValues.ownerNickname,
                   ownerAvatar: post.dataValues.ownerAvatar,
                   ownerRole: post.dataValues.ownerRole,
+                  comments: post.dataValues.comments,
                 }
               })
               .sort((prevPost, nextPost) => {
